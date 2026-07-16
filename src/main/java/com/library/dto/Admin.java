ecce547249658b4f278da4795bd3abbfe07da355
sakshibiradar17 <sakshibiradar760@gmail.com>
2026-07-16 14:43:18 +0530

    class added

--- src/main/java/com/library/dto/Admin.java
+++ src/main/java/com/library/dto/Admin.java
@@ -1,5 +1,63 @@
 package com.library.dto;
 
-public class Admin {
-  private int adminId;
-}
+ public class Admin {
+
+      private int adminId;
+      private String name;
+      private String email;
+      private String phone;
+      private String password;
+
+      public Admin() {
+      }
+
+      public Admin(int adminId, String name, String email, String phone, String password) {
+          this.adminId = adminId;
+          this.name = name;
+          this.email = email;
+          this.phone = phone;
+          this.password = password;
+      }
+
+      public int getAdminId() {
+          return adminId;
+      }
+
+      public void setAdminId(int adminId) {
+          this.adminId = adminId;
+      }
+
+      public String getName() {
+          return name;
+      }
+
+      public void setName(String name) {
+          this.name = name;
+      }
+
+      public String getEmail() {
+          return email;
+      }
+
+      public void setEmail(String email) {
+          this.email = email;
+      }
+
+      public String getPhone() {
+          return phone;
+      }
+
+      public void setPhone(String phone) {
+          this.phone = phone;
+      }
+
+      public String getPassword() {
+          return password;
+      }
+
+      public void setPassword(String password) {
+          this.password = password;
+      }
+
+  }
+
